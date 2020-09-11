ffda019a490207005a5988d55c40d22d50d2fb0b
Dillon Lee <dillon_lee@live.com>
2020-09-11 01:37:04 -0500

    requirements added v5

--- RockyPoolServices/settings.py
+++ RockyPoolServices/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')#mvg#5io0@!jqcg3_w2)=*@g9e@o3906x_(%r+p9w2u&jr^!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['RockysPoolCare.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file, you must be live!")
